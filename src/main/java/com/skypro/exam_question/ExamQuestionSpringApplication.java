c763e54b1facafdff4b69a0d988af36801dd799c
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2023-01-28 12:02:49 +0400

    Initial commit

---
+++ src/main/java/com/skypro/exam_question/ExamQuestionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.exam_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/exam_question/ExamQuestionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.exam_question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
